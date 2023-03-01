24663a94bda5d92175d27c1987ca1049aba9d8cc
techyug <82304969+techyug@users.noreply.github.com>
2023-03-01 18:26:26 +0530

    Added Jwt

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express")
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken')
 var app = express();
+
 app.use(express.json());
-//To parse json data
+app.use(bodyParser.json())
+
 const mysql = require("mysql2")
 const con = mysql.createConnection({
     host: 'localhost',
@@ -49,31 +52,31 @@ app.get('/services', (req, res) => {
         }
     )
 })
-app.get('/services/:id',(req,res)=>{
-    console.log("req::::",req.params)
-    con.query('select ServiceProviderId,ServiceProvideName,ServiceCategoryId from service where service_id=?',[req.params.id],(err,result)=>{
+app.get('/services/:id', (req, res) => {
+    console.log("req::::", req.params)
+    con.query('select ServiceProviderId,ServiceProvideName,ServiceCategoryId from service where service_id=?', [req.params.id], (err, result) => {
         if (err) res.send("error in query" + err)
         else res.send(result[0]);
     })
-   
+
 })
-app.get('/service-providers',(req,res)=>{
-    con.query('select * from SERVICE_PROVIDER',[req.params.id],(err,result)=>{
+app.get('/service-providers', (req, res) => {
+    con.query('select * from SERVICE_PROVIDER', [req.params.id], (err, result) => {
         if (err) res.send("error in query" + err)
         else res.send(result);
     })
 
 })
-app.get('/service-providers/:serviceId',(req,res)=>{
+app.get('/service-providers/:serviceId', (req, res) => {
     console.log("new req")
-    con.query('select * from SERVICE_PROVIDER where ServiceCategoryId=?',[req.params.serviceId],(err,result)=>{
+    con.query('select * from SERVICE_PROVIDER where ServiceCategoryId=?', [req.params.serviceId], (err, result) => {
         if (err) res.send("error in query" + err)
         else res.send(result);
     })
 
 })
-app.get('/service-provider/:serviceProviderId',(req,res)=>{
-    con.query('select * from SERVICE_PROVIDER where ServiceProviderId=?',[req.params.serviceProviderId],(err,result)=>{
+app.get('/service-provider/:serviceProviderId', (req, res) => {
+    con.query('select * from SERVICE_PROVIDER where ServiceProviderId=?', [req.params.serviceProviderId], (err, result) => {
         if (err) res.send("error in query" + err)
         else res.send(result[0]);
     })
@@ -101,7 +104,7 @@ app.post('/users', (req, res) => {
 app.post('/service-provider', (req, res) => {
     // return res.send(req.body);
     con.query(`insert into SERVICE_PROVIDER (ServiceProviderImage,ServiceProvideName,ServiceProviderPhone,ServiceProviderEmail,ServiceProviderPassword)
-    values(?,?,?,?,?);`, [req.body.ServiceProviderImage, req.body.ServiceProvideName, req.body.ServiceProviderPhone, req.body.ServiceProviderEmail,req.body.ServiceProviderPassword],
+    values(?,?,?,?,?);`, [req.body.ServiceProviderImage, req.body.ServiceProvideName, req.body.ServiceProviderPhone, req.body.ServiceProviderEmail, req.body.ServiceProviderPassword],
         (err, result) => {
             if (err) {
                 res.send(err.message)
@@ -111,57 +114,68 @@ app.post('/service-provider', (req, res) => {
             }
         })
 })
-app.post('/service-provider/add-service',(req,res)=>{
+app.post('/service-provider/add-service', (req, res) => {
     res.send("add service to service provider profile");
 })
 app.post("/login", (req, res) => {
     // return res.send(req.body);
 
-    con.query(`select * from service_provider where ServiceProviderPhone=? and ServiceProviderPassword=? `, [req.body.user_phone,req.body.user_pass],(err,result)=>{
-        if(err)
-        (err)=>console.log("error ",err)
-        else if(result[0]!=undefined){
+    
+    con.query(`select * from  service_provider left join
+     service_provider_and_service on 
+     service_provider.ServiceProviderId=service_provider_and_service.ServiceProviderIdinMap 
+     where ServiceProviderPhone=${con.escape(req.body.user_phone)} 
+     and ServiceProviderPassword=${con.escape(req.body.user_pass)} `, 
+     (err, result) => {
+        if (err)
+            console.log("error ", err)
+        else if (result[0] != undefined) {
             result[0].role = "Service Provider";
-            result[0].msg="Login Success"
-            console.log(result[0])
+            result[0].msg = "Login Success"
+            let token = jwt.sign(result[0], "Provider", { expiresIn: '1h' })
+            result[0].token = token;
             res.send(result[0]);
+
         }
-        else{
-            con.query("select * from user where user_phone=? and user_pass=?", [req.body.user_phone, req.body.user_pass], (err, result) => {
+        else {
+            con.query(`select * from user where user_phone=${con.escape(req.body.user_phone)} and user_pass=${con.escape(req.body.user_pass)}`, (err, result) => {
                 if (err) {
+                    console.log(err)
                     res.end("Error : " + err);
                 }
                 else {
                     if (result[0]) {
-                        result[0].role="user";
-                        result[0].msg="Login Success";
+                        result[0].role = "user";
+                        result[0].msg = "Login Success";
+                        let token = jwt.sign(result[0], "User", { expiresIn: '1h' })
+                        result[0].token = token;
                         res.send(result[0])
                     }
                     else {
-                        res.json({msg:"Wrong password"})
-                        
+                        res.json({ msg: "Wrong password" })
+
                     }
                 }
             })
 
         }
-        
+
 
     })
-   
-    
+
+
 })
-app.get('/search-service/:searchtearm',(req,res)=>{
+app.get('/search-service/:searchtearm', (req, res) => {
     con.query(`select * from service where service_title  like '%${req.params.searchtearm}%'`,
-    (err, result) => {
-        if(err){
-            console.log("error : ",err);
-        }else{
-            res.send(result);
+        (err, result) => {
+            if (err) {
+                console.log("error : ", err);
+            } else {
+                res.send(result);
+            }
         }
-    }
     )
-    
+
 })
 app.get('/super', (req, res) => {
     con.query("select * from super_cat", (err, result) => {
@@ -182,7 +196,7 @@ app.post('/super', (req, res) => {
 })
 
 
-app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.send("Hello world");
 })
@@ -194,4 +208,4 @@ app.get('/hello', (req, res) => {
     res.send("sdkefl");
 })
 
-app.listen(3000, '192.168.153.180', console.log("server running"));
+app.listen(3000, '192.168.162.79', console.log("server running"));
